7eefd1bfa807452a932734eaf34cef0f7fec269e
RomanRudin <58559283+RomanRudin@users.noreply.github.com>
2024-01-31 11:38:54 +0200

    Update ex_2_2.py

--- Lab 1/ex_2_2.py
+++ Lab 1/ex_2_2.py
@@ -2,15 +2,20 @@ n = int(input("Please, enter the n number: "))
 print()
 
 max_string = ""
-for i in range(1, n + 1):
+for i in range(n, 0, -1):
     max_string += str(i)
-max_len = len(max_string) 
+for i in range(2, n + 1):
+    max_string += str(i)
+extra_digits = len(max_string) - n * 2 - 1
+print(extra_digits)
 
 for i in range(1, n + 1):
     line = ""
     for j in range(i, 0, -1):
-        line += str(j)
-    line = " " * (max_len - len(line)) + line   
+        line += str(j) 
     for j in range(2, i + 1):
         line += str(j) 
+    line = " " * (extra_digits - len(line)) + line  
+    extra_digits -= (len(str(i)) - 1) * 2
     print(line)
+    print(extra_digits)
