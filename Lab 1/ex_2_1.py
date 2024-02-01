933ec7c47f1b13a837cf5e52f89677adbb8b019e
RomanRudin <rudin.roman@bk.ru>
2024-02-01 16:31:10 +0200

    First lab mostly finished, .giighnore added

--- Lab 1/ex_2_1.py
+++ Lab 1/ex_2_1.py
@@ -1,15 +1,9 @@
-n = int(input("Please, enter the n: "))
+# n = int(input("Please, enter the n: "))
+n = 21
 print()
-
-max_string = ""
-for i in range(1, n + 1):
-    max_string += str(i)
-extra_digits = len(max_string) - n
 
 for i in range(1, n+1):
     line = ""
     for j in range(1, i + 1):
         line += str(j)
-    line = " " * extra_digits + line 
-    extra_digits -= len(str(i)) - 1
     print(line)
