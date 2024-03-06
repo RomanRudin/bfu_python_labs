b7a979fe6e718fb0f19eddd2c34ed9adfd4c68de
RomanRudin <58559283+RomanRudin@users.noreply.github.com>
2024-03-06 11:20:32 +0200

    Create 2.py

---
+++ basic/Lab 3/2.py
@@ -0,0 +1,14 @@
+a = input()
+answer = ""
+i = 0
+while i < len(a):
+	if i < len(a) - 1:
+		if a[i + 1].isdigit():
+			answer += a[i] * int(a[i + 1])
+			i+= 1
+		else:
+			answer += a[i]
+	else:
+		answer += a[i]
+	i += 1
+print(answer)
