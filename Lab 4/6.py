c47e96dc4e7611f1bb2f9de09d2cc092f4654b0c
unknown <rudin.roman@bk.ru>
2024-03-26 21:15:09 +0200

    Mistake in forth lab fixed

--- Lab 4/6.py
+++ Lab 4/6.py
@@ -1,8 +1,16 @@
-#data = input("Please, enter the text: ").replace(',.!?:;"()\t\n', '').split()
+#symbols = ',.!?:;"()\t\n'
+#data = input("Please, enter the text: ")
+#for sym in symbols:
+#    data = data.replace(sym, '')
+#data = data.split()
 #answer = sorted(set(data))
 #answer = sorted(answer, key=lambda word: data.count(x), reverse=True)
 #answer = '\n'.join(answer)
 #print('The asnwer is:', answer, sep='\n')
 
-data = input("Please, enter the text: ").replace(',.!?:;"()\t\n', '').split()
+symbols = ',.!?:;"()\t\n'
+data = input("Please, enter the text: ")
+for sym in symbols:
+    data = data.replace(sym, '')
+data = data.split()
 print('The asnwer is:\n' +'\n'.join(sorted(sorted(set(data)), key=lambda word: data.count(word), reverse=True)))
