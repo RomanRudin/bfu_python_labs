c727744dd6c81433c300af867dfa6bd784bd4f2e
RomanRudin <58559283+RomanRudin@users.noreply.github.com>
2024-05-02 14:05:36 +0200

    Update 7 lab

--- Lab 7/5.py
+++ Lab 7/5.py
@@ -3,7 +3,29 @@ import scipy
 from math import *
 #Function for multivariate normal is:
 # exp( - 0.5 * (x - m)T * m ** -1 * (x - m)) / sqrt(2 * pi ** rank(c) * det(c))
+def timer(function):
+    def wrapper(function):
+      pass
 
-np.exp(0 - 0.5 * (x - m).T * np.linalg.inv(m) * (x - m)) / sqrt((2 * pi) ** np.linalg.matrix_rank(c) * np.linalg.det(c))
+@timer
+def multivariate_normal_logpdf(x, m, c):
+  """
+  Computes the logarithmic density of a multivariate normal distribution.
 
-scipy.stats.multivariate_normal(m, C).logpdf(X)
+  Args:
+    x: A numpy array of shape (n, d), where n is the number of data points and d is the dimensionality of the distribution.
+    mean: A numpy array of shape (d,) representing the mean of the distribution.
+    cov: A numpy array of shape (d, d) representing the covariance matrix of the distribution.
+
+  Returns:
+    A numpy array of shape (n,) containing the logarithmic density of each data point.
+  """
+  
+  quad_form = np.einsum('ij,ij->i', x - mean, np.dot(np.linalg.inv(c), x - mean))
+  logpdf = -0.5 * (np.log(2 * np.pi) + np.log(np.linalg.det(c)) + quad_form)
+  return logpdf
+
+
+@timer
+def using_scipy(m, c, x):
+    return scipy.stats.multivariate_normal(m, C).logpdf(x)
