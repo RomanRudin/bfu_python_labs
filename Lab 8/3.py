9859e63819e62f43b6ad3d88783b4473120f9e1f
Roman <rudin.roman@bk.ru>
2024-05-25 12:15:52 +0200

    A slight change in 3rd lab was made to make graphics look better

--- Lab 8/3.py
+++ Lab 8/3.py
@@ -4,14 +4,14 @@ from matplotlib.animation import FuncAnimation
 
 figure = plt.figure()
 
-ratio = np.linspace(0, 1, num=100)
+ratio = np.linspace(0, 10, num=1000)
 t = np.linspace(-4, 4, num=500)
 x = np.array([np.sin(i * t) for i in ratio])
-y = np.array(list(np.sin(t) for _ in ratio))
+y = np.array(list(np.sin(10 * t) for _ in ratio))
 
 def animate(i):
     figure.clear()
     plt.plot(x[i], y[i])
 
-anim = FuncAnimation(figure, animate, frames=100, interval=10)
+anim = FuncAnimation(figure, animate, frames=1000, interval=10)
 plt.show()
